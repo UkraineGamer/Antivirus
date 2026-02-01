d69ff2391afc4677934329f57992230c0a696615
UkraineGamer <andrey.pervak@gmail.com>
2026-02-01 14:17:41 +0000

    Initial setup of project files and directory structure.

---
+++ duplicate.py
@@ -0,0 +1,26 @@
+import os
+import subprocess
+import sys
+from pathlib import Path
+
+def folder_search():
+    folder_path = str(input("Enter the folder path: "))
+    path = Path(folder_path)
+
+    choice = str(input("Do you want the full path? (y/n): "))
+    if choice == "y":
+        for file in path.iterdir():
+            print(file)
+    else:
+        for file in path.iterdir():
+            print(f"{file.parent.name}/{file.name}")
+    folder_search_menu()
+
+def folder_search_menu():
+    choice = str(input("Do you want to continue searching? (y/n): "))
+    if choice == "y":
+        folder_search()
+    else:
+        print("Exiting...")
+
+folder_search()
